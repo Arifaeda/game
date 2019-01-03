325be41b68bf7ef44b0845eea4f0eba8fabfce18
死の金属と数学。 <>
2019-01-03 18:38:47 -0500

    Changed title spacing, added spacing, took the ranks/elite/places to README.md, changed the z input to just entering the space-bar and enter

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Independent-Game
-Crystalline Realm a small text based rpg
+Crystalline Realm is a small text-based RPG.
+**Please note that things will likely change during the development of the game.
+
+*Current skeleton of the ranks, places, etc below:*
+
+Weakness and strength (rank system):
+-------------------------
+
+Jade < Garnet
+
+Garnet < Opal
+
+Opal < Sapphire
+
+Sapphire < Moonstone
+
+Moonstone < Rose Quartz
+
+Rose Quartz < Tanzanite
+
+Tanzanite < Diamond
+
+Diamond < Jade
+
+Places:
+--------------------------
+
+Gale Plateau
+
+Lykos Taiga
+
+Glaecon Volcano
+
+Thyella Sea Cliff
+
+Nefeloma Lake
+
+Titania's Forest
+
+Elder Woods
+
+Lumina City
+
+Elites:
+--------------------------
+
+Zephyr - Support
+
+Vulpia - Warrior
+
+Hephael - Warrior
+
+Nyssa - Warrior
+
+Diane - Support
+
+Roselia - Support
+
+Alaric - Warrior
+
+Lucille - Tank
+
--- main.py
+++ main.py
@@ -1,106 +1,80 @@
-#Title Screen
-print(" ____ _____ __  __ ____  _____  ____   _     _     _  __  _  ____    _____  ____   ____   _     __  __ ")   
-print("/ (__`| () )\ \/ /(_ (_`|_   _|/ () \ | |__ | |__ | ||  \| || ===|   | () )| ===| / () \ | |__ |  \/  |")   
-print("\____)|_|\_\ |__|.__)__)  |_| /__/\__\|____||____||_||_|\__||____|   |_|\_\|____|/__/\__\|____||_|\/|_|")
+# Title Screen.
 
-"""
-Weakness and strengths.
--------------------------
+print("| ======================================================================================================= |")
+print("|  ____ _____ __  __ ____  _____  ____   _     _     _  __  _  ____    _____  ____   ____   _     __  __  |")
+print("| / (__`| () )\ \/ /(_ (_`|_   _|/ () \ | |__ | |__ | ||  \| || ===|   | () )| ===| / () \ | |__ |  \/  | |")
+print("| \____)|_|\_\ |__|.__)__)  |_| /__/\__\|____||____||_||_|\__||____|   |_|\_\|____|/__/\__\|____||_|\/|_| |")
+print("| ======================================================================================================= |")
+print("                                                                                                       ")
+print("                               Use the space bar to progress in the game.                                    ")
+print("                                                                                                       ")
 
-Jade < Garnet
-Garnet < Opal
-Opal < Sapphire
-Sapphire < Moonstone
-Moonstone < Rose Quartz
-Rose Quartz < Tanzanite
-Tanzanite < Diamond
-Diamond < Jade
-
-Places.
---------------------------
-
-Gale Plateau
-Lykos Taiga
-Glaecon Volcano
-Thyella Sea Cliff
-Nefeloma Lake
-Titania's Forest
-Elder Woods
-Lumina City
-
-Elites.
---------------------------
-
-Zephyr-Support
-Vulpia-Warrior
-Hephael-Warrior
-Nyssa-Warrior
-Diane-Support
-Roselia-Support
-Alaric-Warrior
-Lucille-Tank
-"""
 next_line = ""
 
 # Opening text.
-welcome = input("Welcome to the Crystalline Realm! (press z to continue)")
+welcome = input("Welcome to the Crystalline Realm! ")
 while True:
-  if welcome == "z" or "Z":
-    break
-  else:
-    welcome = input("Welcome to the Crystalline Realm! (press z to continue)")
+    if welcome == " ":
+        break
+    else:
+        welcome = input("Welcome to the Crystalline Realm! ")
 
 # INTRODUCTION:
 
 # Naming:
-name = input("Hello Mystic! You have stumbled upon the Crystalline Realm! May I ask, what is your name?")
+name = input("Hello Mystic! You have stumbled upon the Crystalline Realm! May I ask, what is your name? ")
 
 while True:
-  yes_no_name = input(f'Ah, your name is {name}, did I get that right? [y/n]')
-  if yes_no_name == "y":
-    
-    break
-  if yes_no_name == "n":
-    name = input("What is your name?")
+    yes_no_name = input(f'Ah, your name is {name}, did I get that right? [y/n] ')
+    if yes_no_name == "y" or "Y" or "yes" or "YES":
+        break
+    if yes_no_name == "n" or "N" or "no" or "NO":
+        name = input("What is your name?")
 
 # Dialogue
-player_element_before = input("In this realm, every mystic has a birthstone or crystal, their crystals are infused with elemental magic,their stone determines what abilities they are able to wield and bend. It seems like you don't have one... (press z to continue)")
+player_element_before = input(
+    "In this realm, every mystic has a birthstone or crystal, their crystals are infused with elemental magic, their stone determines what abilities they are able to wield and bend. It seems like you don't have one... ")
 while True:
-  if player_element_before == "z" or "Z":
-    break
-  else:
-    player_element_before = input("In this realm, every mystic has a birthstone or crystal, their crystals are infused with elemental magic,their stone determines what abilities they are able to wield and bend. It seems like you don't have one... (press z to continue)")
-player_element_before2 = input("...luckily for you, I brought some stones with me, pick one, choose wisely... (press z to continue)")
+    if player_element_before == " ":
+        break
+    else:
+        player_element_before = input(
+            "In this realm, every mystic has a birthstone or crystal, their crystals are infused with elemental magic,their stone determines what abilities they are able to wield and bend. It seems like you don't have one... ")
+player_element_before2 = input(
+    "...luckily for you, I brought some stones with me, pick one, choose wisely... ")
 while True:
-  if player_element_before2 == "z" or "Z":
-    break
-  else:
-    player_element_before2 = input("...luckily for you, I brought some stones with me, pick one, choose wisely... (press z to continue)")
+    if player_element_before2 == " ":
+        break
+    else:
+        player_element_before2 = input(
+            "...luckily for you, I brought some stones with me, pick one, choose wisely... ")
 
 # Crystal Selection
 while True:
-  player_crystal = input("  Jade  Rose-quartz  Sapphire  Celestite  Garnet  Tanzanite  Diamond  Opal ")
- 
-  # Wind and Earth abilities
-  if player_crystal == "Jade" or "jade":
-    yes_no_jade = input("Jade: a light green stone engraved with carvings of dragons, swirls, and spirals. Choose this crystal? [y/n]")
-    if yes_no_jade == "Y" or "y":
-      pass
-    if yes_no_jade == "N" or "n":
-      yes_no_jade = ""
-      
-    # Fairytale/Charm abilities
-  if player_crystal.upper == "ROSE-QUARTZ" or "ROSE QUARTZ":
-    yes_no_rosequartz = input("Rose-quartz: a light pink opaque crystal, it somehow looks... cute. Choose this crystal? [y/n]")
-    if yes_no_rosequartz == "Y" or "y":
-      pass
-    if yes_no_rosequartz == "N" or "n":
-      yes_no_rosequartz == ""
-      player_element = input("  Jade  Rose-quartz  Sapphire  Celestite  Garnet  Tanzanite  Diamond  Opal ")
-      
-  # Storm abilities (electric and water)
-  # Cosmic abilities
-  # Tamer (Summon animals) and morphing abilities
-  # Dark/Necromancy/Demonic abilities
-  # Light/Metal abilities
-  # Fire and Ice abilities
+    player_crystal = input("  Jade ~ Rose-quartz ~ Sapphire ~ Celestite ~ Garnet ~ Tanzanite ~ Diamond ~ Opal ")
+
+    # Wind and Earth abilities
+    if player_crystal == "Jade" or "jade":
+        yes_no_jade = input(
+            "Jade: a light green stone engraved with carvings of dragons, swirls, and spirals. Choose this crystal? [y/n] ")
+        if yes_no_jade == "Y" or "y" or "yes" or "YES":
+            pass
+        if yes_no_jade == "N" or "n" or "no" or "NO":
+            yes_no_jade = ""
+
+        # Fairytale/Charm abilities
+    if player_crystal.upper == "ROSE-QUARTZ" or "ROSE QUARTZ":
+        yes_no_rosequartz = input(
+            "Rose-quartz: a light pink opaque crystal, it somehow looks... cute. Choose this crystal? [y/n] ")
+        if yes_no_rosequartz == "Y" or "y" or "yes" or "Yes" or "YES":
+            pass
+        if yes_no_rosequartz == "N" or "n" or "no" or "No" or "NO":
+            yes_no_rosequartz == ""
+            player_element = input("  Jade ~ Rose-quartz ~ Sapphire ~ Celestite ~ Garnet ~ Tanzanite ~ Diamond ~ Opal ")
+
+    # Storm abilities (electric and water)
+    # Cosmic abilities
+    # Tamer (Summon animals) and morphing abilities
+    # Dark/Necromancy/Demonic abilities
+    # Light/Metal abilities
+    # Fire and Ice abilities
