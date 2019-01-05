7488be02814fcce318fb32416d7d5037c658a316
Lunaarii <43122482+Ariel-R-Cepeda@users.noreply.github.com>
2019-01-04 21:37:53 -0500

    Update Dialogue Skip System
    The new dialogue skip system (DSS) has been changed from z to space

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print("                                                                         
 
 
 
-next_line = ""
+
 
 # Opening text.
 welcome = input("Welcome to the Crystalline Realm! ")
@@ -23,9 +23,88 @@ while True:
         break
     else:
         welcome = input("Welcome to the Crystalline Realm! ")
+    
 
 # INTRODUCTION:
 
+# Introduction dialogue
+
+
+lightdialogue1 = input("???: I hope this worked...")
+while True:
+  if lightdialogue1 == " ":
+    break
+  else:
+    lightdialogue1 = input("???: I hope this worked...")
+
+
+lightdialogue2 = input("???: Is anyone there?")
+while True:
+  if lightdialogue2 == " ":
+    break
+  else:
+    lightdialogue2 = input("???: Is anyone there?")
+
+
+lightdialogue3 = input("???: Hello? Can you hear me?")
+while True:
+  if lightdialogue3 == " ":
+    break
+  else:
+    lightdialogue3 = input("???: Hello? Can you hear me?")
+
+
+lightdialogue4 = input("???: Can you see me?")
+while True:
+  if lightdialogue4 == " ":
+    break
+  else:
+    lightdialogue4 = input("???: Can you see me?")
+
+
+thought1= input("*you slowly try to open your eyes, but even squinting, the mysterious bright white light is blinding")
+while True:
+  if thought1 == " ":
+    break
+  else:
+    thought1 = input("*you slowly try to open your eyes, but even squinting, the mysterious bright white light is blinding")
+
+
+lightdialogue5= input("???: Huh, I guess not...")
+while True:
+  if lightdialogue5 == " ":
+    break
+  else:
+    lightdialogue5 = input("???: Huh, I guess not...")
+
+
+lightdialogue6= input("???: Anyhow, there is no time to lose.")
+while True:
+  if lightdialogue6 == " ":
+    break
+  else:
+    lightdialogue6 = input("???: Anyhow, there is no time to lose.")
+
+
+lightdialogue7= input("???: This realm is sinking into grave darkness, they are in need of a great hero to prevent such a fate...")
+while True:
+  if lightdialogue7 == " ":
+    break
+  else:
+    lightdialogue7 = input("???: This realm is sinking into grave darkness, they are in need of a great hero to prevent such a fate...")
+
+
+lightdialogue8= input("???: The imbalance of light and dark is spinning out of control, you are this realm's last hope.")
+while True:
+  if lightdialogue8 == " ":
+    break
+  else:
+    lightdialogue8 = input("???: The imbalance of light and dark is spinning out of control, you are this realm's last hope.")
+
+
+    
+    
+    
 # Naming:
 name = input("Hello Mystic! You have stumbled upon the Crystalline Realm! May I ask, what is your name? ")
 
@@ -35,6 +114,9 @@ while True:
         break
     if yes_no_name == "n" or "N" or "no" or "NO":
         name = input("What is your name?")
+        
+        
+        
 
 # Dialogue
 player_element_before = input(
